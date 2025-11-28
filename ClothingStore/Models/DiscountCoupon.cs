5061fa2fb5b330b8bfd5b1cb8dcf27d755f48609
Thinh161 <thinh.bdd03685@sinhvien.hoasen.edu.vn>
2025-11-28 20:02:08 +0700

    update table admin

---
+++ ClothingStore/Models/DiscountCoupon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClothingStore.Models;
+
+public partial class DiscountCoupon
+{
+    public int CouponId { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public decimal? DiscountPercent { get; set; }
+
+    public DateOnly? ExpiryDate { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ ClothingStore/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClothingStore.Models;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public decimal? TotalAmount { get; set; }
+
+    public int? CouponId { get; set; }
+
+    public string? PaymentMethod { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual DiscountCoupon? Coupon { get; set; }
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+    public virtual User? User { get; set; }
+}
